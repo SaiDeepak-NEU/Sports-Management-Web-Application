fa58e52043c1bf13940862b87d2558025838e136
saideepak <dogiparthi.sai@northeastern.edu>
2022-10-14 19:15:57 -0400

    login validation added

--- Login.js
+++ Login.js
@@ -1,3 +1,21 @@
 function OnLogin() {
+
+    credentials = {
+        "cricket@gmail.com" : "Cricket",
+        "badminton@yahoo.com" : "Badminton",
+        "football@outlook.com" : "Football"
+    }
+
+    var username = document.getElementById('username');
+    var password = document.getElementById('password').value;
+
+    if(password == credentials[username.value] ) {        
+
     window.location.href = "/Home.html";
+
+    } else {
+        alert('Invalid Username or Password');
+    }
+
+
 }
