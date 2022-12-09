eedd93eb2f256ff613b966d44c62b5848c7725dd
ananthakrishna-kalle-neu <kalle.a@northeastern.edu>
2022-12-09 02:15:24 -0500

    Changed logo in Side Drawer and Footer Changes

--- Playbook_SportsApp/client/src/components/layout/Footer.js
+++ Playbook_SportsApp/client/src/components/layout/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { AppBar, Container, Grid, Link, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
 
 import Logo from '../../img/logop.png';
+import './Footer.css';
 
 export default() => {
     return(
@@ -41,6 +49,12 @@ export default() => {
                             Privacy
                         </Link>
                     </Grid>
+                    <div className='social-container'>
+                    <a href="#" className="youtube social"><FontAwesomeIcon icon={faYoutube} size="2x" /></a>
+                    <a href="#" className="facebook social"><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
+                    <a href="#" className="twitter social"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+                    <a href="#" className="instagram social"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+                    </div>
                     <Grid item xs={12} sm={3}></Grid>
                     <Grid item xs={12} sm={9}>
                         <Typography className="marginL-d7">
